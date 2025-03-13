4e0b2147fc179f37ccd090d760ceed8afb63c828
larsmertens2 <lars_mertens@student.uhasselt.be>
2025-03-13 14:43:51 +0100

    Delete build-systems/c/friendship-tester-c-start directory

--- build-systems/c/friendship-tester-c-start/src/friendshiptester.h
+++
@@ -1,8 +0,0 @@
-#ifndef FRIENDSHIPTESTER_H
-#define FRIENDSHIPTESTER_H
-
-// Bereken de vriendschapscore op basis van twee namen.
-// De score ligt tussen 0 en 100.
-double calculate_friendship(const char *name1, const char *name2);
-
-#endif // FRIENDSHIPTESTER_H
